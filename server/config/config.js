740864012b5829fe8eb5563194eb80935e9998f2
Alejandro <jeffersonalejandro27@gmail.com>
2020-11-23 21:37:21 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if ( process.env.NODE_ENV === 'dev'){
-    urlDB = 'mongodb://localhost:27017/cafe'
-}else{
-    urlDB = 'mongodb+srv://Alava-own:1234@cluster-1.nio54.mongodb.net/cafe'
-}
+    if ( process.env.NODE_ENV === 'dev'){
+        urlDB = 'mongodb://localhost:27017/cafe'
+    }else{
+        urlDB = process.env.MONGO_URI; // MONGO_URI es una variable de entorno que creamos desde la consola y lo que guarda es el link de la coneccion de la base de datos y la ocultamos por que este link tiene el usuario y la constraseña de nuestra BD; (Este proceso lo anote en el cuaderno en la parte de gitHub)
+    }
 
 process.env.URLDB = urlDB;  //URLDB  es un nombre creado en esta linea
 
